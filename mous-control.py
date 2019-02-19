62bf828205a87efd1095b93f5db49a3fbe75d050
i am moein <44437189+moeinsk@users.noreply.github.com>
2019-02-19 03:57:33 -0800

    Add files via upload

---
+++ mous-control.py
@@ -0,0 +1,23 @@
+#writed by moein
+import pyautogui
+
+
+# print(20*"-")
+print(pyautogui.size())
+
+print(pyautogui.position())
+# print(20*"-")
+
+
+pyautogui.rightClick(1293,83)
+pyautogui.moveTo(1107,300)
+pyautogui.click(1107,300)
+print("It works//")
+pyautogui.doubleClick(952,300)
+
+
+
+#❤This is Python's power❤️
+#enjoy it
+
+
